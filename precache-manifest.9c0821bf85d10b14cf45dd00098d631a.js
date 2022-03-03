62d59360351e8d20be5a8b737ec50bd191b7234b
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-02 17:00:49 -0800

    Updates

---
+++ precache-manifest.9c0821bf85d10b14cf45dd00098d631a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3449e84b99b1d985053f",
+    "url": "/ReactPortfolio/static/css/main.d3814b81.chunk.css"
+  },
+  {
+    "revision": "3449e84b99b1d985053f",
+    "url": "/ReactPortfolio/static/js/main.3449e84b.chunk.js"
+  },
+  {
+    "revision": "e6942a101afa606b1b87",
+    "url": "/ReactPortfolio/static/js/1.e6942a10.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "a11a05832e71679ce98cc216dbc90f80",
+    "url": "/ReactPortfolio/static/media/Resume.a11a0583.pdf"
+  },
+  {
+    "revision": "b96233ce73fb5577928243c5ef8377ac",
+    "url": "/ReactPortfolio/static/media/Dobie_big.b96233ce.jpg"
+  },
+  {
+    "revision": "6a4cf4853c511fba1f5b33b38e31f7cd",
+    "url": "/ReactPortfolio/static/media/background.6a4cf485.jpg"
+  },
+  {
+    "revision": "243e735a20b695bb439b9ea894276c0e",
+    "url": "/ReactPortfolio/static/media/codingQuiz.243e735a.png"
+  },
+  {
+    "revision": "279cd8b706d57f40975cd846bd7d15d8",
+    "url": "/ReactPortfolio/static/media/gitCooking.279cd8b7.png"
+  },
+  {
+    "revision": "09283494c7785444519a2438da69decc",
+    "url": "/ReactPortfolio/static/media/horiseon.09283494.png"
+  },
+  {
+    "revision": "b7bfea5d1a217f9ab7f6b6527b0533d6",
+    "url": "/ReactPortfolio/static/media/passwordGenerator.b7bfea5d.png"
+  },
+  {
+    "revision": "409dc5afaf5d231b191df70497fc0f68",
+    "url": "/ReactPortfolio/static/media/portfolio.409dc5af.png"
+  },
+  {
+    "revision": "40d60617f06ed3b7a95b12f3aff16aeb",
+    "url": "/ReactPortfolio/static/media/shadeMarketPlace.40d60617.png"
+  },
+  {
+    "revision": "2ef6c9f4d14bfbf2033583aaa1bbcb88",
+    "url": "/ReactPortfolio/static/media/weatherApplication.2ef6c9f4.png"
+  },
+  {
+    "revision": "e13998e05a82abe760059a1f48a1a765",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
